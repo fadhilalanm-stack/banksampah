6617f5e468c4614145b01844fa758e1995faf5c7
fadhilalanm-stacku <fadhilalanm@gmail.com>
2026-06-12 18:30:10 +0700

    HujanBroo

--- src/bank_sampah/modules/login/LoginController.java
+++ src/bank_sampah/modules/login/LoginController.java
@@ -16,7 +16,7 @@ public class LoginController {
         LoginDAO.UserLogin user = dao.login(username, password);
         if (user != null) {
             Session.setUser(user.idUser, user.nama, user.username, user.role);
-            AlertUtil.info("Login Berhasil", "Selamat datang di Sistem Informasi Bank Sampah. APAPUN YANG MANAUDHRHRBR");
+            AlertUtil.info("Login Berhasil", "Selamat datang di Sistem Informasi Bank Sampah. DISINI HUJAN BROOO");
             MainApp.showAdminPage("dashboard");
         } else {
             AlertUtil.error("Login Gagal", "Username atau password salah.");
