162b5437f3a671d532ab0eca2a25b3f808ecd886
aishaara <162643932+nurchol@users.noreply.github.com>
2026-06-12 21:20:43 +0700

    Update TopbarView

--- src/bank_sampah/layout/TopbarView.java
+++ src/bank_sampah/layout/TopbarView.java
@@ -10,6 +10,10 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.Separator;
+import javafx.geometry.Orientation;
 
 public class TopbarView {
     private final HBox topbar;
@@ -19,7 +23,6 @@ public class TopbarView {
         this.activeMenu = activeMenu;
         topbar = new HBox(15);
         topbar.getStyleClass().add("topbar");
-        topbar.setPadding(new Insets(15, 25, 15, 25));
         topbar.setAlignment(Pos.CENTER_LEFT);
         build();
     }
@@ -27,20 +30,29 @@ public class TopbarView {
     private void build() {
         TextField search = new TextField();
         search.setPromptText("Cari data...");
-        search.setPrefWidth(260);
         search.getStyleClass().add("search-field");
 
         Region spacer = new Region();
         HBox.setHgrow(spacer, Priority.ALWAYS);
 
-        Label notif = new Label("🔔");
-        notif.getStyleClass().add("topbar-icon");
+        ImageView notifIcon = new ImageView(
+            new Image("file:/C:/ProjectJava/Praktikum/img/notifikasi.png")
+        );
+        notifIcon.getStyleClass().add("topbar-icon");
+        
+        ImageView settingIcon = new ImageView(
+            new Image("file:/C:/ProjectJava/Praktikum/img/pengaturan.png")
+        );
+        settingIcon.getStyleClass().add("topbar-icon");
 
-        Label setting = new Label("⚙");
-        setting.getStyleClass().add("topbar-icon");
+        Separator divider = new Separator();
+        divider.setOrientation(Orientation.VERTICAL);
+        divider.getStyleClass().add("topbar-divider");
 
         VBox userBox = new VBox(2);
-        userBox.setAlignment(Pos.CENTER_RIGHT);
+        userBox.setAlignment(Pos.CENTER_LEFT);
+
+        HBox.setMargin(userBox, new Insets(0, 16, 0, 0));
 
         Label name = new Label(Session.getNama());
         name.getStyleClass().add("topbar-name");
@@ -50,10 +62,20 @@ public class TopbarView {
 
         userBox.getChildren().addAll(name, role);
 
-        Circle avatar = new Circle(18);
+        ImageView avatar = new ImageView(
+            new Image("file:/C:/ProjectJava/Praktikum/img/pp_admin.png")
+        );
+        
+        avatar.setFitWidth(50);
+        avatar.setFitHeight(50);
+        avatar.setPreserveRatio(false);
+        
+        Circle clip = new Circle(25, 25, 25);
+        avatar.setClip(clip);
+        
         avatar.getStyleClass().add("avatar");
 
-        topbar.getChildren().addAll(search, spacer, notif, setting, userBox, avatar);
+        topbar.getChildren().addAll(search, spacer, notifIcon, settingIcon, divider, avatar, userBox);
     }
 
     public HBox getView() {
