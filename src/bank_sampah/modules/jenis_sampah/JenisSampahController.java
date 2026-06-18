d1d34e289c7efad4aaec7421af7de42c528af0d7
aishaara <162643932+nurchol@users.noreply.github.com>
2026-06-18 20:31:26 +0700

    Update JenisSampahController

--- src/bank_sampah/modules/jenis_sampah/JenisSampahController.java
+++ src/bank_sampah/modules/jenis_sampah/JenisSampahController.java
@@ -1,5 +1,9 @@
 package bank_sampah.modules.jenis_sampah;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import bank_sampah.database.DBConnection;
 import bank_sampah.util.AlertUtil;
 import javafx.collections.ObservableList;
 
@@ -21,6 +25,53 @@ public class JenisSampahController {
             AlertUtil.warning("Input Salah", "Harga harus berupa angka.");
         } catch (Exception e) {
             AlertUtil.error("Gagal", "Data gagal disimpan: " + e.getMessage());
+        }
+    }
+
+    public double rataRataHarga() {
+        double total = 0;
+        int jumlah = getData().size();
+        
+        if (jumlah == 0) {
+            return 0;
+        }
+        
+        for (Sampah s : getData()) {
+            total += s.getHargaPerKg();
+        }
+        
+        return total / jumlah;
+    }
+
+    public void hapus(Sampah sampah) {
+        String sql = "DELETE FROM jenis_sampah WHERE id_sampah = ?";
+        
+        try (Connection conn = DBConnection.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+            
+            ps.setInt(1, sampah.getIdSampah());
+            ps.executeUpdate();
+        
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(Sampah sampah, String nama, String kategori, String harga) {
+        String sql = "UPDATE jenis_sampah SET nama_sampah = ?, kategori = ?, harga_per_kg = ? WHERE id_sampah = ?";
+        
+        try (Connection conn = DBConnection.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql)) {
+            
+            ps.setString(1, nama);
+            ps.setString(2, kategori);
+            ps.setDouble(3, Double.parseDouble(harga));
+            ps.setInt(4, sampah.getIdSampah());
+            
+            ps.executeUpdate();
+        
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
