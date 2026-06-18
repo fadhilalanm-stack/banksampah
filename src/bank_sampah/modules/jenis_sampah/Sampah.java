201fd5439821bdcdd6fd4c8a790b0f5cc0766654
aishaara <162643932+nurchol@users.noreply.github.com>
2026-06-18 20:36:24 +0700

    Update Sampah.java

--- src/bank_sampah/modules/jenis_sampah/Sampah.java
+++ src/bank_sampah/modules/jenis_sampah/Sampah.java
@@ -1,20 +1,58 @@
 package bank_sampah.modules.jenis_sampah;
 
 public class Sampah {
+
     private int idSampah;
     private String namaSampah;
     private String kategori;
     private int hargaPerKg;
 
-    public Sampah(int idSampah, String namaSampah, String kategori, int hargaPerKg) {
+    public Sampah(
+            int idSampah,
+            String namaSampah,
+            String kategori,
+            int hargaPerKg
+    ) {
+
         this.idSampah = idSampah;
         this.namaSampah = namaSampah;
         this.kategori = kategori;
         this.hargaPerKg = hargaPerKg;
     }
 
-    public int getIdSampah() { return idSampah; }
-    public String getNamaSampah() { return namaSampah; }
-    public String getKategori() { return kategori; }
-    public int getHargaPerKg() { return hargaPerKg; }
+    // ================= GETTER =================
+
+    public int getIdSampah() {
+        return idSampah;
+    }
+
+    public String getNamaSampah() {
+        return namaSampah;
+    }
+
+    public String getKategori() {
+        return kategori;
+    }
+
+    public int getHargaPerKg() {
+        return hargaPerKg;
+    }
+
+    // ================= SETTER =================
+
+    public void setIdSampah(int idSampah) {
+        this.idSampah = idSampah;
+    }
+
+    public void setNamaSampah(String namaSampah) {
+        this.namaSampah = namaSampah;
+    }
+
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+
+    public void setHargaPerKg(int hargaPerKg) {
+        this.hargaPerKg = hargaPerKg;
+    }
 }
