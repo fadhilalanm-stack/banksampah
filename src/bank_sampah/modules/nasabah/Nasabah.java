5e6faf6e131e509d1b847cbdf539d1ab48bb0b0d
aishaara <162643932+nurchol@users.noreply.github.com>
2026-06-19 14:54:24 +0700

    Update nasabah.java

--- src/bank_sampah/modules/nasabah/Nasabah.java
+++ src/bank_sampah/modules/nasabah/Nasabah.java
@@ -1,6 +1,7 @@
 package bank_sampah.modules.nasabah;
 
 public class Nasabah {
+
     private int idNasabah;
     private String kodeNasabah;
     private String nama;
@@ -9,7 +10,16 @@ public class Nasabah {
     private int poin;
     private String status;
 
-    public Nasabah(int idNasabah, String kodeNasabah, String nama, String alamat, String noHp, int poin, String status) {
+    public Nasabah(
+            int idNasabah,
+            String kodeNasabah,
+            String nama,
+            String alamat,
+            String noHp,
+            int poin,
+            String status
+    ) {
+
         this.idNasabah = idNasabah;
         this.kodeNasabah = kodeNasabah;
         this.nama = nama;
@@ -19,11 +29,63 @@ public class Nasabah {
         this.status = status;
     }
 
-    public int getIdNasabah() { return idNasabah; }
-    public String getKodeNasabah() { return kodeNasabah; }
-    public String getNama() { return nama; }
-    public String getAlamat() { return alamat; }
-    public String getNoHp() { return noHp; }
-    public int getPoin() { return poin; }
-    public String getStatus() { return status; }
+    // ================= GETTER =================
+
+    public int getIdNasabah() {
+        return idNasabah;
+    }
+
+    public String getKodeNasabah() {
+        return kodeNasabah;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getNoHp() {
+        return noHp;
+    }
+
+    public int getPoin() {
+        return poin;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    // ================= SETTER =================
+
+    public void setIdNasabah(int idNasabah) {
+        this.idNasabah = idNasabah;
+    }
+
+    public void setKodeNasabah(String kodeNasabah) {
+        this.kodeNasabah = kodeNasabah;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setNoHp(String noHp) {
+        this.noHp = noHp;
+    }
+
+    public void setPoin(int poin) {
+        this.poin = poin;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
